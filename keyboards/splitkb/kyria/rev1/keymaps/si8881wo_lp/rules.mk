6c95bea1bf402cc43153a38fd2d32eabea23aadf
si8881wo <68005613+si8881wo@users.noreply.github.com>
2024-02-11 17:26:17 +0100

    added my custom keymaps for kyria (hp and lp) and 0xc_pad

---
+++ keyboards/splitkb/kyria/rev1/keymaps/si8881wo_lp/rules.mk
@@ -0,0 +1,4 @@
+OLED_ENABLE = yes
+ENCODER_ENABLE = no       # Enables the use of one or more encoders
+RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
